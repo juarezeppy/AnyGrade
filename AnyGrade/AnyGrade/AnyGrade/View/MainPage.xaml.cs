2ae30af938a3e5de1acf32fc3d9c50369df494ac
juarezeppy <jejuarez88@gmail.com>
2017-03-10 02:44:07 -0800

    First Commit
    Main page layout mostly complete

---
+++ AnyGrade/AnyGrade/AnyGrade/View/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using AnyGrade.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AnyGrade
+{
+    public partial class MainPage : ContentPage
+    {
+        MainPageViewModel vm;                               //create a vm object so the view can grab its data
+
+        public MainPage()
+        {
+            vm = new MainPageViewModel();                  //initialize the vm object by using its constructor to get course object info
+            BindingContext = vm;
+
+            InitializeComponent();
+        }
+
+        public void OnItemTapped(object o, ItemTappedEventArgs e)
+        {
+            var crse = e.Item as Course;
+            DisplayAlert("Selection made", "You tapped on " + crse.name, "OK");
+        }
+    }
+
+
+}
---
+++ AnyGrade/AnyGrade/AnyGrade/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyGrade.ViewModel
+{
+    public class MainPageViewModel
+    {
+        //this constructor takes a class object 
+        //and then initializes its own member variable with it
+
+        //public MainPageViewModel(Course course)
+        //{
+        //    courseName = course.name;
+        //}
+//        public string courseName { get; set; }
+
+        public List<Course> Courses {
+            get {
+                return new List<Course>() {
+                    new Course() {name = "ICS 6D" },
+                    new Course() {name = "CS 122A"},
+                    new Course() {name = "ICS 139W"}
+                };
+            }
+        }
+
+
+        /*
+         * This method creates and returns a course object
+         * 
+         * var course allocates memory for the object then initializes
+         * member variable
+         */
+        public static Course GetCourse()
+        {
+            var course = new Course()
+            {
+                name = "COMP SCI"
+            };
+
+            return course;                  
+        }
+
+    }
+}
